ec2eb9f03c169a28a51fc64e8a321a6a297d6327
inmor <mailarnisu@seznam.cz>
2025-08-02 12:55:32 +0200

    Внёс тестовое изменение. Домашка вопросы.

--- src/HomeTask1.java
+++ src/HomeTask1.java
@@ -30,13 +30,69 @@
                }
            }
            System.out.println( count);*/
-           Scanner in = new Scanner(System.in);
+           /*Scanner in = new Scanner(System.in);
            int n = in.nextInt();
            long sum = 0;
            for ( int i = 0; i < n; i++){
                int m = in.nextInt();
                sum += m;
            }
-           System.out.println( "Сумма эл-тов равна " + sum  );
+           System.out.println( "Сумма эл-тов равна " + sum  );*/
+           /*int product = 0;
+           for( int i = 1; i<=10; i++){
+             for( int j = 1; j<=10; j++) {
+                  product = i * j;
+                 System.out.printf("%4d", product);
+
+             }
+               System.out.println();
+
+             }*/
+           /*Scanner in = new Scanner( System.in);
+           System.out.println(" Введите кол-во элементов массива:  ");
+            int n = in.nextInt();
+           int[] nums = new int[n];
+           int sum = 0;
+           System.out.println("Введите "+ n + " целых чисел: ");
+            for( int i = 0; i < n; i++){
+                nums[i] = in.nextInt();
+                sum += nums[i];
+            }
+           System.out.println("Сумма всех элементов массива равна " + sum);*/
+           /*Scanner in = new Scanner(System.in);
+           System.out.println("Введите целое число: ");
+           int n = in.nextInt();
+           int sumEven = 0;
+           int sumOdd = 0;
+
+           for( int i = 0; i<=n; i++ ){
+               if( i %2 == 0){
+                   sumEven += i;
+               }
+               else{
+                   sumOdd += i;
+               }
+
+           }
+           System.out.println("Сумма чётных чисел равна: " + sumEven + " Сумма нечётных чисел равна: "  + sumOdd);*/
+
+           Scanner in = new Scanner(System.in);
+           System.out.println("Ведите целое число ");
+           int n = in.nextInt();
+           int prev = 1;
+           boolean hasEven = false;
+           for( int i =2; i<=n; i+=2){
+               prev *= i;
+               hasEven = true;
+
+           }
+           System.out.println(hasEven ? prev:0);
+
+
+
+           
+
+
+
        }
    }
--- src/WorkHome130725.java
+++ src/WorkHome130725.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class WorkHome130725 {
     public static void main(String[] args) {
 
-        Scanner in = new Scanner(System.in);
+        /*Scanner in = new Scanner(System.in);
         int n;
         System.out.println("введите число n ");
         n = in.nextInt();
@@ -10,8 +10,26 @@ public class WorkHome130725 {
             if (( i %5 == 0)&&( i %3 ==0))
                 System.out.println( i );
 
+        }*/
+        Scanner in = new Scanner(System.in);
+        int n;
+        System.out.println("Введите с клавиатуры число n ");
+        n = in.nextInt();
+        System.out.println("Ведите вариант: ");
+        char f = in.next().charAt(0);// Чтение варианта
+        switch (f)// Выбор варианта ( перебор вариантов).
+        {
+            case'а': // В случае варианта "а"
+                for( int i = 0; i < n; i++)
+                {
+                    for( int j = 0; j < n; j++)
+                        if(i <= j)  // идёт проверка того, что эл-т нах-ся выше гл.диагонали.
+                            System.out.print("*" + "\t");// элементы выше главной диагонали выводится в виде *
+                        else
+                            System.out.print(" " + "\t");// Остальные выводятся в виде пробела.
+                    System.out.println();
+                }
         }
     }
-
 }
 
